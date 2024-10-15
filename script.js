2e55348609ac4130b40e26a89e1999366d28653a
frlopez88 <68876977+frlopez88@users.noreply.github.com>
2024-10-14 20:13:01 -0600

    Code Fixed

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const endpoint = "https://dfypyzoplyzfywjnitcn.supabase.co/rest/v1/movierental"
 const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRmeXB5em9wbHl6Znl3am5pdGNuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjgzNDczMTksImV4cCI6MjA0MzkyMzMxOX0.vANaYlT4x8hX9wjS_Yy2v4sOH-3OTJa577cAj_MAdXQ"
 
+let modal = bootstrap.Modal(document.getElementById(""))
+
 async function getmovies() {
 
     let response = await fetch(endpoint, {
@@ -10,31 +12,39 @@ async function getmovies() {
             "Authorization": token,
             "Content-Type": `application/json`
         },
-        
+
     })
 
     let data = await response.json()
-    console.log(data)
+    rendermovies(data)
 }
 
 getmovies()
 
 function rendermovies(data) {
     let tablelayout = `<tr>
-                        <th>title</th>
-                        <th>genre</th>
-                        <th>releasedate</th>
-                        <th>available</th>
-                        <tr>`
-
+                <th>title</th>
+                <th>genre</th>
+                <th>releasedate</th>
+                <th>available</th>
+                <th>Edit</th>
+                <th>Delete</th>
+                </tr>`
     for (let i = 0; i < data.length; i++) {
         tablelayout += `<tr>
-                            <td>${data[i].title}</td>
-                            <td>${data[i].genre}</td>
-                            <td>${data[i].releasedate}</td>
-                            <td>${data[i].available}</td>
-                            <tr>`
+                        <td>${data[i].title}</td>
+                        <td>${data[i].genre}</td>
+                        <td>${data[i].releasedate}</td>
+                        <td>${data[i].available}</td>
+                        <td> <button onclick="showmodal(${data[i].id})
+                    "class="btn btn-warning">edit</button> </td>
+                    <td> 
+                    <button onclick=deletemovie(${data[i].id}) class="btn btn-danger">Delete</button></td>
+
+            </tr>`
+                    
     }
+
     reportmovies.innerHTML = tablelayout
 }
 
@@ -56,29 +66,32 @@ async function postmovie() {
 
 
 
-let response = await fetch(endpoint, {
-    method: `POST`,
-    headers: {
-        "apikey": token,
-        "Authorization": token,
-        "Content-Type": `application/json`
-    },
+    let response = await fetch(endpoint, {
+        method: `POST`,
+        headers: {
+            "apikey": token,
+            "Authorization": token,
+            "Content-Type": `application/json`
+        },
 
 
-})
+    })
 
 
-if (response.status >= 200 && response.status <= 299) {
-    console.log("object created")
-    getmovies()
-} else {
-    
+    if (response.ok) {
+        console.log("object created")
+        getmovies()
+    } else {
+        console.log("object was not created")
+    }
+
 }
 
-}
+async function showmodal(id) {
+    let url = `${endpoint}?id=eq.${id}`
 
-async function showmodal(title) {
-    let url = `${endpoint}?id=eq${title}`
+
+    console.log(url)
     let response = await fetch(url, {
         method: `GET`,
         headers: {
@@ -94,26 +107,31 @@ async function showmodal(title) {
         inputgenre.value = data[0].genre
         inputreleasedate = data[0].releasedate
         inputavailable = data[0].available
+
+        
     } else {
+        console.log("Movie could not be fetched")
+        let data = await response.json();
+        console.log(data)
+    }
+}
 
+async function patchcontact(id) {
+
+    let title = inputtitle2.value
+    let genre = inputgenre2.value
+    let releasedate = inputreleasedate2.value
+    let available = inputavailable2.value
+
+    let url2 = `${endpoint}?id=eq${id}`
+
+    let jsonbody = {
+        title,
+        genre,
+        releasedate,
+        available
     }
 
-    async function patchcontact() {
-
-        let title = inputtitle2.value
-        let genre = inputgenre2.value
-        let releasedate = inputreleasedate2.value
-        let available = inputavailable2.value
-
-        let url2 = `${endpoint}?id=eq${title}`
-
-        let jsonbody = {
-            title,
-            genre,
-            releasedate,
-            available
-        }
-    }
 
     let response2 = await fetch(url, {
         method: `PATCH`,
@@ -127,62 +145,29 @@ async function showmodal(title) {
 
     if (response.ok) {
         console.log("object has been updated")
-    } else
+    } else {
         console.log("object hasnt been updated")
-    let responsebody = await response.json()
-    console.log(responsebody)
-
-    async function showmodal(id = 1) {
-        let url = `${endpoint}?id=eq.${id}`
-
-        let response = await fetch(url, {
-            method: `GET`,
-            headers: {
-                "apikey": token,
-                "Authorazation": token
-            }
-        })
-        modal.show()
-    }
-
-    async function deletecontact(id) {
-        let url = `${endpoint}?id=eq.${id}`
-
-        let response = await fetch(url, {
-            method: `DELETE`,
-            headers: {
-                "apikey": token,
-                "Authorization": token
-
-            }
-        })
-    }
-
-    function rendermovies(data){
-        let tablelayout = `<tr>
-                <th>title</th>
-                <th>genre</th>
-                <th>releasedate</th>
-                <th>available<t/h>
-                <tr>`
-                for(let i=0; i < datalength;i++){
-                    tablelayout += `<tr>
-                        <td>${data[i].title}</td>
-                        <td>${data[i].genre}</td>
-                        <td>${data[i].releasedate}</td>
-                        <td>${data[i].available}</td>
-                    <td> <button onclick="showmodal(${data[i].title})
-                    "class="btn btn-warning">edit</button> </td>
-                    <td> <button onclick="showmodal(${data.genre})
-                    "class=btn btn-danger">edit></button></td>
-
-            </tr>
-                    reportmovies.innerHTML= tablelayout`
-                }
-    }
-    function showmodal(){
-        let modal = new bootstrap.modal(document.getElementById)("moviemodal")
-    modal.show()
+        let responsebody = await response.json()
+        console.log(responsebody)
     }
 }
 
+async function deletemovie(id) {
+    let url = `${endpoint}?id=eq.${id}`
+
+    let response = await fetch(url, {
+        method: `DELETE`,
+        headers: {
+            "apikey": token,
+            "Authorization": token
+
+        }
+    })
+
+    if (response.ok){
+        console.log("Object Deleted")
+        getmovies()
+    }else{
+        console.log("Object was not deleted")
+    }
+}
